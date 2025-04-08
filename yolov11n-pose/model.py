b90d10a0303f7065307a2588b55d769ef49cda53
Semenovykh Kirill <semenovykh.ka@phystech.edu>
2025-04-08 15:50:28 +0300

    infer code for yolo

--- yolov11n-pose/model.py
+++ yolov11n-pose/model.py
@@ -1,6 +1,12 @@
 from ultralytics import YOLO
 import cv2
+import fire
 
-if __name__ == "__main__":
+def main(path_to_video="../../report/youtube1.mp4", path_to_result="."):
     model = YOLO("yolo11n-pose.pt")
     model.eval()
+    results = model.predict(source=path_to_video, show=False, save=True, save_dir=path_to_result)
+
+if __name__ == "__main__":
+    fire.Fire(main)
+    main()
